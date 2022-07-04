742679b6f3dc2440cecef251941e06e0104a6bc2
Adroaldo Barbosa da Paixão Junior <adroaldo@gruporecursos.intranet>
2022-07-03 16:44:35 -1200

    continua

--- CadastroPessoa/Scripts/funcoes.js
+++ CadastroPessoa/Scripts/funcoes.js
@@ -80,3 +80,59 @@ function aplicarCor() {
     
     paragrafo.style.color = cor;
 }
+
+//function verificarNome() {
+//    var nome = document.getElementById('nome').value;
+
+//    while (nome != 'Junior') {
+//        document.getElementById('resposta').innerHTML = 'O nome digitado não é junior';
+//    }
+//}
+
+//verificarNome();
+
+function mostrarMarcasDeCarro() {
+    var marcas = ['Volkswagem', 'Chevrolet', 'Ford', 'Honda', 'Toyota', 'Mitsubishi'];
+
+    var i = 0;
+
+    while (i < marcas.length) {
+        document.write(marcas[i] + '<br>');
+        i++;
+    }
+
+    //marcas.forEach(function (item) {
+    //    document.write(item + '<br>');
+    //});
+
+    //for (var i = 0; i < marcas.length; i++) {
+    //    document.write(marcas[i] + '<br>');
+    //}
+}
+
+function sortearMarcas() {
+    var marcas2 = ['Volkswagem', 'Chevrolet', 'Ford', 'Honda', 'Toyota', 'Mitsubishi'];
+    var max = marcas2.length - 1;
+    var min = 0;
+    var valor = Math.random() * (max - min) + min;
+    return marcas2[Math.floor(valor)];
+}
+
+function sortearEnquantoNaoSairToyota() {
+    document.getElementById('marcas2').innerHTML = '';
+
+    var marcaSorteada = sortearMarcas();
+    var marcaDesejada = document.getElementById('marcaDesejada').value;
+
+    while (marcaSorteada != marcaDesejada) {
+        //console.log('Marca sorteada: ' + marcaSorteada);
+        document.getElementById('marcas2').innerHTML += 'Marca sorteada: ' + marcaSorteada + '<br>';
+        marcaSorteada = sortearMarcas();
+    }
+
+    document.getElementById('marcas2').innerHTML += 'Saiu a: ' + marcaSorteada;
+}
+
+// quando eu ja sabe o número de repetições, deve-se usar o FOR
+// quando quero fazer a repetição para cada item de uma lista = FOREACH
+// quando quero fazer algo até uma determinada condição ser sastifeita, usa o WHILE
